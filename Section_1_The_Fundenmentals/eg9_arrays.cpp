ab1f8bfa226edf6fc0fba5af548fbecb8d38cb5c
s24hao <70073970+s24hao@users.noreply.github.com>
2021-05-02 15:43:22 -0400

    adding array

---
+++ Section_1_The_Fundenmentals/eg9_arrays.cpp
@@ -0,0 +1,28 @@
+
+void eg_1_9(){
+    //arrays are ALWAYS passed by reference
+    //the address of the reference of an array is it's 0th element
+    /////////////////////////////!
+    // Be very careful about OUT OF BOUND for arrays
+    // arr[-1], arr[len(arr)] contains other useful data for this and other programs
+    /////////////////////////////!
+    //declaration:
+    //type identifier[number of elements];
+    int arr[10];
+
+    //definition or initialization:
+    for (int i=0;i<sizeof(arr)/sizeof(arr[0]); i++){
+        arr[i] = i;
+    }
+
+    //size of arr:
+    int sz = sizeof(arr)/sizeof(arr[0]);
+
+    //There is no way to append to an array
+    //There is no way to assign the size of the array as a variable
+}
+
+int main(){
+    eg_1_9();
+    return 0;
+}
